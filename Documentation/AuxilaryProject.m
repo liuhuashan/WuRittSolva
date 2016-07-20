62415e548c2eb1531f2b4a65c7e32ed5ea095f54
Liu Hua-Shan <liukaitianpidi@sina.com>
2016-07-21 04:13:31 +0800

    Delete AuxilaryProject.m

--- Documentation/AuxilaryProject.m
+++
@@ -1,8 +0,0 @@
-{"Name" -> "WuRittSolva", "Directory" -> "C:\\Program Files\\Wolfram \
-Research\\Mathematica\\5.0\\AddOns\\Autoload\\WuRittSolva\\Documentation\\Eng\
-lish\\", "Files" -> {"A Collection of Testing Problems.nb", 
-   "Demonstration of WuRittSolva in Elementary Geometry.nb", 
-   "Demostration of WuRittSolva.nb", "Details of WuRittSolva.nb", "WuRittSolv\
-a for Concrete Geometric Configurations in Elementary Geometry.nb", 
-   "WuRittSolva Tools.nb", "WuRittSolva UserGuide.nb", 
-   "WuRittSolva User Manual.nb"}}
